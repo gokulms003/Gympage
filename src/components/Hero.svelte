6519e9ef8d4a642b390696b92640f99252f5f231
gokulms003 <130273491+gokulms003@users.noreply.github.com>
2024-06-30 12:18:47 +0530

    Add files via upload

---
+++ src/components/Hero.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Stars from "./+Stars.svelte";
+import CtAs from "./CTAs.svelte";
+    import Header from "./Header.svelte";
+    import SectionWrapper from "./SectionWrapper.svelte";
+</script>
+
+<SectionWrapper>
+    <Header />
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center">
+            <span class="text-indigo-400">Gym</span>Training <span class="text-slate-600 line-through">Is Hard</span><br/>
+            Just Got <span class="text-indigo-400">Easier</span>
+        </h2>
+        <p class="text-xl sm:text-2xl ms:text-3xl text-center max-w-[1000px] mx-full">
+            Less thinking & more doing. we're the trainer in your pocket full of <span class="italic">Personalized workouts,exercises,explanations,analytics</span> and much much more.
+        </p>
+        <CtAs/>
+       <Stars />
+    </div>
+</SectionWrapper>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script>
+    import "../app.css";
+  import CtAs from "../components/CTAs.svelte";
+    import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+
+    import {openModel} from "../store";
+
+    let y
+    $: outerHeight = 0
+
+    function reroute(href) {
+        $openModel = false;
+        window.location.href = href;
+    }
+  </script>
+  
+  {#if $openModel}
+    <div class="fixed top-0 left-0 w-screen h-screen border-b  bg-white z-50 flex flex-col gap-8 p-5 px-8 md:hidden">
+      <div class="flex items-center jsutify-between gap-4 border-b pb-2">
+        <h1 class="font-semibold">Swoley 
+          <span class="text-indigo-400">Moley</span>
+        </h1>
+        <button on:click={() => $openModel=false} class="ouutline-none border-none">
+          <i class="fa-solid fa-xmark text-2xl"></i>
+        </button>
+      </div>
+      <div class="flex flex-col gap-4">
+        <button on:click={() => reroute('#Product')} class="border-none outline-none p-2 group duration-200 cursor-pointer text-left">
+          <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">Product <i class="fa-solid fa-chevron-right text-xl pl-4" /></p>
+        </button>
+        <button on:click={() => reroute('#Reviews')} class="border-none outline-none p-2 group duration-200 cursor-pointer text-left">
+          <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">Reviews <i class="fa-solid fa-chevron-right text-xl pl-4" /></p>
+        </button>
+        <button on:click={() => reroute('#Faqs')} class="border-none outline-none p-2 group duration-200 cursor-pointer text-left">
+          <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">FAQs <i class="fa-solid fa-chevron-right text-xl pl-4" /></p>
+        </button>
+      </div>
+      <div class="flex flex-col items-center justify-center"></div>
+      <CtAs/>
+    </div> 
+  {/if}
+
+  {#if y > outerHeight}
+    <div class="bg-white fixed top-0 left-0 w-full flex flex-col z-20 px-4 fadeIn">
+      <Header/>
+    </div>
+  {/if}
+  <slot />
+
+  <Footer/>
+  <svelte:window bind:scrollY={y} bind:outerHeight/>
